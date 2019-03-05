a2303e145018b15baa8841b6e053d0258c502ea0
kumarpreyasi <42677948+kumarpreyasi@users.noreply.github.com>
2019-03-04 23:11:04 -0800

    Update Product.java

--- Cash Register/Product.java
+++ Cash Register/Product.java
@@ -1,1 +1,26 @@
+public class Product {
+
+    //Create Product class
+    //Create a class that will contain the information for a simple product in a grocery store.
+    // Each instance of the class will contain a name of the item, a PLU or UPC code number, price, type of item, quantity available, and if the item is eligible for sales tax.
+
+    String name;
+
+    int code; //PLU or UPC
+
+    double price;
+
+    String type;
+
+    int QuantAvailable;
+
+    Boolean SalesTax;
+    
+
+    public Product() {
+
+
+    }
+
+}
 
