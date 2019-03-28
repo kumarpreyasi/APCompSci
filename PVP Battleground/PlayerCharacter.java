3e2e92b96c4a6b6ba70976179e948866e789a7da
kumarpreyasi <42677948+kumarpreyasi@users.noreply.github.com>
2019-03-28 08:33:01 -0700

    Update PlayerCharacter.java

--- PVP Battleground/PlayerCharacter.java
+++ PVP Battleground/PlayerCharacter.java
@@ -1,12 +1,31 @@
-public class PlayerCharacter {
+public abstract class PlayerCharacter {
 
-    String name;
+    protected String name;
 
-    int strength, intelligence, agility, hp, maxhp, mp, maxmp;
+    protected int strength, intelligence, agility, hp, maxhp, mp, maxmp;
+
+    private static int count;
 
     public PlayerCharacter() {
 
+        System.out.println("A new PlayerCharacter!");
+        count++;
 
     }
+
+    public void showStats() {
+
+        System.out.printf(" HP: %3d/%3d\n", hp, maxhp);
+        System.out.printf(" MP: %3d/%3d\n", mp, maxmp);
+        System.out.printf("STR: %3d\nINT: %3d\nAGI: %3d\n",strength,intelligence,agility);
+
+    }
+
+    public static int numCharacters() {
+
+        return count;
+
+    }
+
 
 }
