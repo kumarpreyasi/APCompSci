ad4707b304ba1c945967226658d4786dc2bcd710
kumarpreyasi <42677948+kumarpreyasi@users.noreply.github.com>
2019-03-28 08:33:47 -0700

    Update Mage.java

--- PVP Battleground/Mage.java
+++ PVP Battleground/Mage.java
@@ -1,38 +1,33 @@
 import java.util.Random;
 
-public class Mage extends PlayerCharacter{
+public class Mage extends PlayerCharacter implements Range {
 
-    int wisdom;
+    private int wisdom;
 
     public Mage(String newName){
 
-        System.out.println("A new Mage has been created!");
-
         name = newName;
+
+        System.out.println("A new Mage, " + name + ", has been created!");
 
         Random rand = new Random();
 
         strength = 7 + (rand.nextInt(6) + 1);
-
         intelligence = 15 + (rand.nextInt(6) + 1);
-
         agility = 8 + (rand.nextInt(6) + 1);
-
         wisdom = 10 + (rand.nextInt(6) + 1);
 
         hp = maxhp = strength;
-
         mp = maxmp = intelligence + (wisdom * 2);
     }
 
     public void showStats() {
+
         System.out.printf("%s, a Mage:\n",name);
 
-        System.out.printf("STR: %3d  INT: %3d  AGI: %3d  WIS: %3d\n",strength,intelligence,agility,wisdom);
+        super.showStats();
 
-        System.out.printf(" HP: %3d/%3d\n", hp, maxhp);
-
-        System.out.printf(" MP: %3d/%3d\n", mp, maxmp);
+        System.out.printf("WIS: %3d\n",wisdom);
     }
 
 
