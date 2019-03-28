737b6432ce5ce3d062ff0caea584707154cdba6d
kumarpreyasi <42677948+kumarpreyasi@users.noreply.github.com>
2019-03-28 08:34:33 -0700

    Update Priest.java

--- PVP Battleground/Priest.java
+++ PVP Battleground/Priest.java
@@ -1,38 +1,33 @@
 import java.util.Random;
 
-public class Priest extends PlayerCharacter{
+public class Priest extends PlayerCharacter implements Healer{
 
-    int charisma;
+    private int charisma;
 
     public Priest(String newName){
 
-        System.out.println("A new Priest has been created!");
-
         name = newName;
+
+        System.out.println("A new Priest, " + name + ", has been created!");
 
         Random rand = new Random();
 
         strength = 7 + (rand.nextInt(6) + 1);
-
         intelligence = 15 + (rand.nextInt(6) + 1);
-
         agility = 8 + (rand.nextInt(6) + 1);
-
         charisma = 10 + (rand.nextInt(6) + 1);
 
         hp = maxhp = strength;
-
         mp = maxmp = intelligence + (charisma * 2);
     }
 
     public void showStats() {
+
         System.out.printf("%s, a Priest:\n",name);
 
-        System.out.printf("STR: %3d  INT: %3d  AGI: %3d  WIS: %3d\n",strength,intelligence,agility,charisma);
+        super.showStats();
 
-        System.out.printf(" HP: %3d/%3d\n", hp, maxhp);
-
-        System.out.printf(" MP: %3d/%3d\n", mp, maxmp);
+        System.out.printf("CHA: %3d\n",charisma);
     }
 
 
