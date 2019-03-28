10bc3e406b3a422c3a74a0d58d15e1b77b061006
kumarpreyasi <42677948+kumarpreyasi@users.noreply.github.com>
2019-03-28 10:11:47 -0700

    Update Product.java

--- Cash Register/Product.java
+++ Cash Register/Product.java
@@ -5,22 +5,30 @@ public class Product {
     // Each instance of the class will contain a name of the item, a PLU or UPC code number, price, type of item, quantity available, and if the item is eligible for sales tax.
 
     String name;
-
-    int code; //PLU or UPC
-
-    double price;
-
     String type;
+    protected int code; //PLU or UPC
+    protected double price;
+    protected Boolean taxEligible;
+    int quantity;
 
-    int QuantAvailable;
-
-    Boolean SalesTax;
-    
-
-    public Product() {
-
+    void setQuantity(int quantity1){
 
     }
 
-}
 
+    public Product(String newName, String newType, int newCode, double newPrice, boolean isTaxEligible, int newQuantity) {
+
+        name = newName;
+        type = newType;
+        code = newCode;
+        price = newPrice;
+        taxEligible = isTaxEligible;
+        quantity = newQuantity;
+
+        System.out.println("There are " + quantity + " " + name + "s available. They cost $" + price + " each. They are in the " + type + " section.");
+
+    }
+
+
+
+}
