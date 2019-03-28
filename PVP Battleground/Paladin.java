090ad9ec3a9a85681af3f81dd17e2986a7cd3d2f
kumarpreyasi <42677948+kumarpreyasi@users.noreply.github.com>
2019-03-28 08:34:06 -0700

    Update Paladin.java

--- PVP Battleground/Paladin.java
+++ PVP Battleground/Paladin.java
@@ -1,40 +1,35 @@
 import java.util.Random;
 
-public class Paladin extends PlayerCharacter{
+public class Paladin extends PlayerCharacter implements Tank, Healer{
 
-    int wisdom, constitution;
+    private int wisdom, constitution;
 
     public Paladin(String newName){
 
-        System.out.println("A new Paladin has been created!");
-
         name = newName;
+
+        System.out.println("A new Paladin, " + name + ", has been created!");
 
         Random rand = new Random();
 
         strength = 10 + (rand.nextInt(6) + 1);
-
         intelligence = 15 + (rand.nextInt(6) + 1);
-
         agility = 5 + (rand.nextInt(6) + 1);
-
         wisdom = 10 + (rand.nextInt(6) + 1);
-
         constitution = 10 + (rand.nextInt(6) + 1);
 
         hp = maxhp = strength + (constitution * 2);
-
         mp = maxmp = intelligence + (wisdom * 2);
     }
 
     public void showStats() {
+
         System.out.printf("%s, a Paladin:\n",name);
 
-        System.out.printf("STR: %3d  INT: %3d  AGI: %3d  CON: %3d\n",strength,intelligence,agility,constitution);
+        super.showStats();
 
-        System.out.printf(" HP: %3d/%3d\n", hp, maxhp);
+        System.out.printf("WIS: %3d\nCON: %3d\n",wisdom,constitution);
 
-        System.out.printf(" MP: %3d/%3d\n", mp, maxmp);
     }
 
 }
